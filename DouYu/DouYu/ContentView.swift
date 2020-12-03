1c9828fbe3566dae1a28e196cbf616f1a34a7c59
yumingming <mingmingyush@wecanintl.com>
2020-12-03 14:14:15 +0800

    斗鱼项目初始化

---
+++ DouYu/DouYu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DouYu
+//
+//  Created by erp on 2020/12/3.
+//  Copyright © 2020 erp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
